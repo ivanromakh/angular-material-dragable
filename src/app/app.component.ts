2639d9908e402b346f97b86d563af2de90e063c2
ivanrouman <ivanrouman@gmail.com>
2018-07-17 13:53:49 +0300

    nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  brandName = 'Example Nav Bar';
+  navLinks = ['Dashboard', 'Home', 'Settings'];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MatButtonModule, MatToolbarModule } from '@angular/material';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      NavbarComponent
+   ],
+   imports: [
+      BrowserModule,
+      BrowserAnimationsModule,
+      LayoutModule,
+
+      MatButtonModule,
+      MatToolbarModule,
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
