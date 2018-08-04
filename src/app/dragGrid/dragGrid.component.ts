43a4b154803f28a58b96354fb27c9e20de737608
ivanrouman <ivanrouman@gmail.com>
2018-08-04 16:23:54 +0300

    !!version without bugs!!

--- src/app/dragGrid/dragGrid.component.ts
+++ src/app/dragGrid/dragGrid.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 import { trigger, state, style, transition, animate } from '../../../node_modules/@angular/animations';
 
 @Component({
@@ -6,28 +6,27 @@ import { trigger, state, style, transition, animate } from '../../../node_module
   templateUrl: './dragGrid.component.html',
   styleUrls: ['./dragGrid.component.css'],
   animations: [
-    trigger('heroState', [
-      state('inactive', style({
-        backgroundColor: '#eee',
-        transform: 'scale(1)'
-      })),
-      state('active',   style({
-        backgroundColor: '#cfd8dc',
-        transform: 'scale(1.1)'
-      })),
-      transition('inactive => active', animate('100ms ease-in')),
-      transition('active => inactive', animate('100ms ease-out'))
+    trigger('dragElement', [
+      transition('* => *', animate(
+        '500ms ease',
+        style({ left: '{{left}}', top: '{{top}}'})), {params: {left: 'auto', top: 'auto'}}
+      )
+    ]),
+    trigger('rotation', [
+      transition('* => *', animate('500ms ease', style({transform: 'rotate(90)'})))
     ])
   ]
 })
+
 export class DragGridComponent implements OnInit {
-  hoverCard = null;
   dropCard = null;
+  dragDuration = 500;
+
   cards = [
-    { title: 'Card 1', cols: 1, rows: 1 },
-    { title: 'Card 2', cols: 1, rows: 1 },
-    { title: 'Card 3', cols: 1, rows: 1 },
-    { title: 'Card 4', cols: 1, rows: 1 }
+    { title: 'Card 1', state: 'stand', params: {x: 0, y: 0}, cols: 1, rows: 1, x: 0, y: 0},
+    { title: 'Card 2', state: 'stand', params: {x: 0, y: 0}, cols: 1, rows: 1, x: 0, y: 0},
+    { title: 'Card 3', state: 'stand', params: {x: 0, y: 0}, cols: 1, rows: 1, x: 0, y: 0},
+    { title: 'Card 4', state: 'stand', params: {x: 0, y: 0}, cols: 1, rows: 1, x: 0, y: 0}
   ];
 
   constructor() { }
@@ -39,17 +38,53 @@ export class DragGridComponent implements OnInit {
     this.dropCard = card;
   }
 
-  onMouseMoveGrid(card) {
-    this.hoverCard = card;
+  getMatGridTile(element) {
+    let tries = 10;
+    while (element.tagName !== 'MAT-GRID-TILE') {
+      element = element.parentElement;
+      tries --;
+      if (tries < 0) {
+        break;
+      }
+    }
+    return element;
+  }
+
+  setCardPositionForAnimation(card, gridTile) {
+    card.params.x = gridTile.offsetLeft + 'px';
+    card.params.y = gridTile.offsetTop + 'px';
+  }
+
+  onMouseDownTile() {
+    console.log(123);
+  }
+
+  onMouseMoveGrid(card, event) {
+    const gridTile = this.getMatGridTile(event.target);
+    this.setCardPositionForAnimation(card, gridTile);
 
     if (!this.dropCard) {
       return;
     }
-    if (this.hoverCard !== this.dropCard) {
-      const hoverIndex = this.cards.indexOf(this.hoverCard);
-      const dropIndex = this.cards.indexOf(this.dropCard);
-      this.cards.splice(hoverIndex, 1, this.dropCard);
-      this.cards.splice(dropIndex, 1, this.hoverCard);
+
+    if (card !== this.dropCard) {
+      const {cards, dropCard} = this;
+      card.state = !card.state;
+      dropCard.state = !dropCard.state;
+
+      card.params.left = dropCard.params.x;
+      card.params.top = dropCard.params.y;
+
+      dropCard.params.left = card.params.x;
+      dropCard.params.top = card.params.y;
+
+      setTimeout(function() {
+        const hoverIndex = cards.indexOf(card);
+        const dropIndex = cards.indexOf(dropCard);
+
+        cards.splice(hoverIndex, 1, dropCard);
+        cards.splice(dropIndex, 1, card);
+      }, this.dragDuration);
       this.dropCard = null;
     }
     this.dropCard = null;
