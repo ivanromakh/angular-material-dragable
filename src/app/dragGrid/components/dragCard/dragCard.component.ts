18dda24ceed856148526a1569f3a46e43912e1f6
ivanrouman <ivanrouman@gmail.com>
2018-08-07 17:03:15 +0300

    other drag methd

--- src/app/dragGrid/components/dragCard/dragCard.component.ts
+++ src/app/dragGrid/components/dragCard/dragCard.component.ts
@@ -1,19 +1,51 @@
-import { Component, OnInit, ElementRef, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, ElementRef, Input, EventEmitter, Output, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-drag-card',
   templateUrl: './dragCard.component.html',
   styleUrls: ['./dragCard.component.css'],
 })
-export class DragCardComponent implements OnInit {
+export class DragCardComponent implements OnInit, OnChanges {
   @Input() card;
+  @Input() changed;
   @Output() drop = new EventEmitter();
+  isLoaded = false;
+  tries = 20;
 
   constructor(private _elementRef: ElementRef) {}
+
+  getMatGridTile(element) {
+    let {tries} = this;
+    while (element.tagName !== 'MAT-GRID-TILE') {
+      element = element.parentElement;
+      tries --;
+      if (tries < 0) {
+        break;
+      }
+    }
+    return element;
+  }
+
+  setCardPositionForAnimation(card, gridTile) {
+    card.params.x = gridTile.offsetLeft + 'px';
+    card.params.y = gridTile.offsetTop + 'px';
+    card.params.width = gridTile.offsetWidth + 'px';
+    card.params.height = gridTile.offsetHeight + 'px';
+  }
+
+  updatePosition() {
+    const el = this._elementRef.nativeElement;
+    this.setCardPositionForAnimation(this.card, this.getMatGridTile(el));
+  }
+
+  ngOnChanges(changes) {
+    //this.updatePosition();
+  }
 
   ngOnInit() {
     const el = this._elementRef.nativeElement;
     el.draggable = 'true';
+    el.style.cursor = 'move';
 
     el.addEventListener('dragstart', (e) => {
       el.classList.add('drag-src');
@@ -25,6 +57,7 @@ export class DragCardComponent implements OnInit {
       el.classList.remove('drag-src');
       this.drop.emit(this.card);
     });
+    this.updatePosition();
   }
 }
 
--- src/app/dragGrid/dragGrid.component.ts
+++ src/app/dragGrid/dragGrid.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, HostBinding } from '@angular/core';
-import { trigger, style, transition, animate } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
+import { trigger, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-drag-grid',
@@ -9,15 +9,17 @@ import { trigger, style, transition, animate } from '@angular/animations';
     trigger('dragElement', [
       transition(
         '* => *',
-        animate('500ms ease', style({ left: '{{left}}', top: '{{top}}'})),
-        {params: {left: 'auto', top: 'auto'}}
-      )
+        animate('500ms ease', keyframes([
+          style({ left: '{{x}}', top: '{{y}}', offset: 0 }),
+          style({ left: '{{left}}', top: '{{top}}', offset: 1 }),
+        ]),
+      ), { params: { x: 'null', left: 'null', y: 'null', top: 'null'}})
     ]),
     trigger('rotation', [
       transition(
         '* => *',
-        animate('200ms ease', style({transform: 'rotate({{rotate}}deg)', overflow: 'hidden'})),
-        {params: {rotate: 0}}
+        animate('{{duration}}ms ease', style({transform: 'rotate({{rotate}}deg)', overflow: 'hidden'})),
+        {params: {rotate: 0, duration: 200}}
       )
     ])
   ]
@@ -38,11 +40,13 @@ export class DragGridComponent implements OnInit {
   rotateCounts = 5;
   rotateSlow = 0.5;
   dragDuration = 500;
+  rotateDuration = 200;
 
   cards = [{
       title: 'Population graph',
       rotate: 0,
-      state: 'stand',
+      display: true,
+      state: false,
       content: {type: 'chart'},
       data: {
         labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
@@ -54,32 +58,35 @@ export class DragGridComponent implements OnInit {
         }
         ]
       },
-      params: {x: 0, y: 0, width: 0, height: 0},
+      params: {x: 0, y: 0, left: 0, top: 0, width: 0, height: 0},
       cols: this.baseColSize,
       rows: this.baseRowSize
     }, {
       title: 'Table',
+      display: true,
       rotate: 0,
-      state: 'stand',
+      state: false,
       content: {type: 'documents'},
-      params: {x: 0, y: 0, width: 0, height: 0},
+      params: {x: 0, y: 0, left: 0, top: 0, width: 0, height: 0},
       cols: this.baseColSize,
       rows: this.baseRowSize
     }, {
       title: 'Importand data',
       rotate: 0,
-      state: 'stand',
+      state: false,
+      display: true,
       content: {type: 'text'},
       data: {text: 'users count: 2'},
-      params: {x: 0, y: 0, width: 0, height: 0},
+      params: {x: 0, y: 0, left: 0, top: 0, width: 0, height: 0},
       cols: this.baseColSize,
       rows: this.baseRowSize
     }, {
       title: 'Card 4',
       rotate: 0,
-      state: 'stand',
+      state: false,
+      display: true,
       content: {type: 'table'},
-      params: {x: 0, y: 0, width: 0, height: 0},
+      params: {x: 0, y: 0, left: 0, top: 0, width: 0, height: 0},
       cols: this.baseColSize,
       rows: this.baseRowSize
     }
@@ -108,10 +115,10 @@ export class DragGridComponent implements OnInit {
   }
 
   setCardPositionForAnimation(card, gridTile) {
-    card.params.x = gridTile.offsetLeft;
-    card.params.y = gridTile.offsetTop;
-    card.params.width = gridTile.offsetWidth;
-    card.params.height = gridTile.offsetHeight;
+    card.params.x = gridTile.offsetLeft + 'px';
+    card.params.y = gridTile.offsetTop + 'px';
+    card.params.width = gridTile.offsetWidth + 'px';
+    card.params.height = gridTile.offsetHeight + 'px';
   }
 
   onMouseDownTile(card) {
@@ -136,57 +143,6 @@ export class DragGridComponent implements OnInit {
     setTimeout(data.rotate, data.duration, data);
   }
 
-  moveTileAside(card, target) {
-    if (target.params.x < card.params.x) {
-      card.params.left = target.params.x;
-    } else {
-      if (card.params.width < target.params.width) {
-        card.params.left = target.params.x + target.params.width / 2;
-      } else { card.params.left = target.params.x - card.params.width / 2; }
-    }
-    card.params.left += 'px';
-  }
-
-  moveTilesAside(card, dropCard) {
-    if (card.params.width !== dropCard.params.width) {
-      this.moveTileAside(dropCard, card);
-      this.moveTileAside(card, dropCard);
-    } else {
-      card.params.left = dropCard.params.x + 'px';
-      dropCard.params.left = card.params.x + 'px';
-    }
-  }
-
-  moveTileUpDown(card, target) {
-    if (target.params.y < card.params.y) {
-      card.params.top = target.params.y;
-    } else {
-      if (card.params.height < target.params.height) {
-        card.params.top = target.params.y + target.params.height / 2;
-      } else { card.params.top = target.params.y - card.params.height / 2; }
-    }
-    card.params.top += 'px';
-  }
-
-  moveTilesUpDown(card, dropCard) {
-    if (card.params.height !== dropCard.params.height) {
-      this.moveTileUpDown(card, dropCard);
-      this.moveTileUpDown(dropCard, card);
-    } else {
-      card.params.top = dropCard.params.y + 'px';
-      dropCard.params.top = card.params.y + 'px';
-    }
-  }
-
-  swapCardsPlaces(data) {
-    const {cards, card, dropCard} = data;
-    const hoverIndex = cards.indexOf(card);
-    const dropIndex = cards.indexOf(dropCard);
-
-    cards.splice(hoverIndex, 1, dropCard);
-    cards.splice(dropIndex, 1, card);
-  }
-
   onMouseMoveGrid(card, event) {
     const gridTile = this.getMatGridTile(event.target);
     this.setCardPositionForAnimation(card, gridTile);
@@ -195,24 +151,38 @@ export class DragGridComponent implements OnInit {
 
     if (card !== this.dropCard) {
       const {cards, dropCard} = this;
-      card.state = !card.state;
-      dropCard.state = !dropCard.state;
+      card.display = false;
+      dropCard.display = false;
 
-      if (card.params.y === card.params.y) {
-        this.moveTilesAside(card, dropCard);
-      }
-      if (card.params.x === dropCard.params.x) {
-        this.moveTilesUpDown(card, dropCard);
-      }
+      const paramsCard = {x: card.params.x, left: dropCard.params.x, y: card.params.y, top: dropCard.params.y};
+      const paramsDropCard = {x: dropCard.params.x, left: card.params.x, y: dropCard.params.y, top: card.params.y};
+      console.log(paramsCard, paramsDropCard);
 
-      if (card.params.height === dropCard.params.height) {
-        card.params.top = dropCard.params.y + 'px';
-        dropCard.params.top = card.params.y + 'px';
-      }
+      let hoverIndex = cards.indexOf(card);
+      let dropIndex = cards.indexOf(dropCard);
 
-      setTimeout(this.swapCardsPlaces, this.dragDuration, {cards: cards, card: card, dropCard: dropCard});
+      cards.splice(hoverIndex, 1, dropCard);
+      cards.splice(dropIndex, 1, card);
+
+      hoverIndex = cards.indexOf(card);
+      dropIndex = cards.indexOf(dropCard);
+
+      setTimeout(() => {
+        this.cards[hoverIndex].params = paramsCard;
+        this.cards[dropIndex].params = paramsDropCard;
+
+        this.cards[hoverIndex].state = !this.cards[hoverIndex].state;
+        this.cards[dropIndex].state = !this.cards[dropIndex].state;
+        this.cards[hoverIndex].display = true;
+        this.cards[dropIndex].display = true;
+      }, 60);
+
       this.dropCard = null;
     }
     this.dropCard = null;
+  }
+
+  allowDrop(event) {
+    event.preventDefault();
   }
 }
